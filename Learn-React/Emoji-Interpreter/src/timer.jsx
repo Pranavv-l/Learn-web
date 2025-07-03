69fb5ec3fd740f3aaadec8053173f11cc7365a50
Pranavv-l <pranavlaji@gmail.com>
2025-07-03 23:14:18 +0530

    Added some light changes

--- Learn-React/Emoji-Interpreter/src/timer.jsx
+++ Learn-React/Emoji-Interpreter/src/timer.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react"
+
 function Timer(){
+    const [chat, setChat] = useState(false)
+    const [time, setTime] = useState(0)
+    useEffect(() => {
+        let id;
+        if(chat){
+            id = setInterval(()=>{
+                setTime(prev => prev + 1)
+             },1000)
+        }
+        return () => {
+            clearInterval(id)
+            console.log("Stopped")
+        }
+    },[chat])
+    
     return(
         <>
         <h1> Timer </h1>
-        <button>Start</button>
+        <p>{time}</p>
+        <button onClick={()=>setChat(!chat)}>Enter chat</button>
         </>
     )
 }
