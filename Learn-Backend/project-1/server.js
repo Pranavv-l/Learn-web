7a88599686cc2e83d18773caefb7b516f7286b4f
Pranavv-l <pranavlaji@gmail.com>
2025-07-04 23:59:09 +0530

    Successfuly completed project-1 make a simple server which sends data and modify it

--- Learn-Backend/project-1/server.js
+++ Learn-Backend/project-1/server.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
 const PORT = 8383
+const data = ['apples']
+app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.sendStatus(200)
+    res.send(`
+        <h1>Homepage</h1>
+        <p>${data}</p>
+        `)
 })
 
+app.get('/api/data',(req,res)=>{
+    res.send(data)
+})
+
+app.post('/api/data', (req,res)=>{
+    const newEntry = req.body
+    data.push(newEntry.name)
+    res.sendStatus(201)
+})
+
+app.delete('/api/data',(req,res)=>{
+    data.pop()
+    console.log("data popped")
+    res.sendStatus(203)
+})
 app.listen(PORT, () => {console.log(`Server has started at ${PORT}`)}) 
